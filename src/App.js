088506ec02efa4ce12d4e1b9e09be7c2fb4dbcc8
Yasir Akbar <yasir.akbar86@hotmail.com>
2019-04-27 13:23:14 +0300

    Routing

--- src/App.js
+++ src/App.js
@@ -2,17 +2,24 @@ import React, { Component } from "react";
 import "./App.css";
 import ButtonAppBar from "./components/ButtonAppBar";
 import MainPage from "./components/MainPage";
+import About from "./components/About";
+import Contact from "./components/Contact";
 import Footer from "./components/Footer";
+import { BrowserRouter as Router, Route, Link} from "react-router-dom";
 
 
 class App extends Component {
   render() {
     return (
+      <Router>
       <div>
-        <ButtonAppBar />
-        <MainPage />
+      <ButtonAppBar />
+        <Route exact path="/" component={MainPage} />
+        <Route path="/about" component={About} />
+        <Route path="/topics" component={Contact} />
         <Footer />
       </div>
+      </Router>
     );
   }
 }
