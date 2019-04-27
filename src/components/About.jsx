edb9249f43172437d0eace96ad74bb369b13209b
Yasir Akbar <yasir.akbar86@hotmail.com>
2019-04-27 14:19:21 +0300

    dropzone done

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,10 +1,48 @@
 import React, { Component } from "react";
+import Dropzone from 'react-dropzone';
+
 
 class About extends Component {
+    constructor() {
+        super();
+        this.onDrop = (files) => {
+          this.setState({files})
+        };
+        this.state = {
+          files: []
+        };
+      }
+    
+    Upload() {
+        alert("File Uploaded!");
+    }
+    
   render() {
+  
+        const files = this.state.files.map(file => (
+          <li key={file.name}>
+            {file.name} - {file.size} bytes
+          </li>
+        ));
+    
     return (
       <div>
-          <h1>About</h1>
+          <h1>Drag and Drop here</h1>
+          <Dropzone onDrop={this.onDrop}>
+        {({getRootProps, getInputProps}) => (
+          <section className="container">
+            <div {...getRootProps({className: 'dropzone'})}>
+              <input {...getInputProps()} />
+              <p>Drag 'n' drop some files here, or click to select files</p>
+            </div>
+            <aside>
+              <h4>Files</h4>
+              <ul>{files}</ul>
+            </aside>
+          </section>
+        )}
+      </Dropzone>
+          <input type="file"/><button onClick={this.Upload()}>Upload</button>
       </div>
     );
   }
