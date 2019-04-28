3145c7a2ad00c416f32049c05b1a9a6e37b0d08e
Yasir Akbar <yasir.akbar86@hotmail.com>
2019-04-28 14:29:39 +0300

    upload fixed

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import "./App.css";
 import ButtonAppBar from "./components/ButtonAppBar";
 import MainPage from "./components/MainPage";
-import About from "./components/About";
+import Upload from "./components/Upload";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import { BrowserRouter as Router, Route} from "react-router-dom";
@@ -15,7 +15,7 @@ class App extends Component {
       <div>
       <ButtonAppBar />
         <Route exact path="/" component={MainPage} />
-        <Route path="/about" component={About} />
+        <Route path="/upload" component={Upload} />
         <Route path="/contact" component={Contact} />
         <Footer />
       </div>
